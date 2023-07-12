26d7ee96ee701ef43a9e7cd52961442f741c5018
Gaurav Singh <57258362+8440Gau@users.noreply.github.com>
2023-07-12 21:10:51 +0530

    operator

---
+++ src/OperatorAndCondition/Operator.java
@@ -0,0 +1,13 @@
+package OperatorAndCondition;
+
+public class Operator {
+public static void main(String[] args) {
+	int num1 =25;
+	int num2 =110;
+	System.out.println("/ ");
+    System.out.println("num1/num2: " + (num2 / num1) );
+    System.out.println("% ");
+    System.out.println("num1%num2: " + (num2 % num1) );
+
+}
+}
---
+++ src/OperatorAndCondition/TernaryOperator.java
@@ -0,0 +1,14 @@
+package OperatorAndCondition;
+
+public class TernaryOperator {
+public static void main(String[] args) {
+	int num1;
+	boolean num2;
+	num1 = 25;
+	
+	num2 = (num1/25 == 1)? true:false;
+	
+	System.out.println(num2);
+	
+}
+}
