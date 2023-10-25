37d47ee06eff43e2a5cb009a9bcd662270e0b5f8
Gaurav Singh <57258362+8440Gau@users.noreply.github.com>
2023-10-25 09:51:17 +0530

    student

---
+++ src/toString/Student.java
@@ -0,0 +1,18 @@
+package toString;
+
+class Student
+ {
+ String name;
+ int rollno;
+ Student(String name, int rollno)
+ {
+ this.name=name;
+ this.rollno=rollno;
+ } public static void main(String args[]){
+ Student s1=new Student("saicharan",101);
+ Student s2=new Student("ashok",102);
+ System.out.println(s1);
+ System.out.println(s1.toString());
+ System.out.println(s2);
+ }
+ }
